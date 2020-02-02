40f823cb3dd5a500cf794e4c6fab5a6a8473019a
painteau <52933590+painteau@users.noreply.github.com>
2020-02-03 00:55:51 +0100

    Code cleaning

--- LiveChecker.py
+++ LiveChecker.py
@@ -1,25 +1,28 @@
 from twitch import TwitchClient
-from config import * 
 import RPi.GPIO as GPIO
 import time
+from config import * 
 
-#starting session
+# Starting session
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT)
-print ("LED off")
+
+# Turning off LED
 GPIO.output(18,GPIO.HIGH)
+print ("LED off")
 
+
+# Preparing Twitch check
 client = TwitchClient(client_id)
-
-#FINDING ID of twitch channel
+# FINDING ID of that twitch channel
 users = client.users.translate_usernames_to_ids(channel_name)
 
 for user in users:
 	Current_ID = user.id
 	Current_Name = user.display_name
 
-# TREATING DATA
+# HANDLING DATA
 channel = client.streams.get_stream_by_user(Current_ID)
 
 if channel is None:
